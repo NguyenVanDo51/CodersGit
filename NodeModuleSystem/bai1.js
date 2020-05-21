ffa0cde47bc47f36b460629a506f2362c08816e6
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2020-05-21 23:03:17 +0700

    Bai tap 1 2 3

---
+++ NodeModuleSystem/bai1.js
@@ -0,0 +1,9 @@
+/**
+ * 1. Require module math
+ * 2. Sử dụng method `add` để tính tổng 2 số bất kì
+ * 3. Log ra kết quả phép tính tổng ở trên
+ */
+var add2 = require("./sum.js").default;
+
+console.log(add2(2, 3));
+console.log(add2(3, 4));
