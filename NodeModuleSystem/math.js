f1669e365c4bc0e5e56605cdc68b696851d05b16
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2020-05-21 23:29:33 +0700

    hihi

---
+++ NodeModuleSystem/math.js
@@ -0,0 +1,14 @@
+var math = {
+  add: function(a, b) {
+    return a + b;
+  }
+};
+
+var multiply = {
+  add: (a, b) => {
+    return a * b;
+  }
+};
+
+module.exports = math;
+module.exports = multiply;
