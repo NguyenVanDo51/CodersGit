2a3530f614f927a061dc645e16dbc4da95e925a4
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2020-05-21 23:40:11 +0700

    aaa

--- NodeModuleSystem/bai1.js
+++ NodeModuleSystem/bai1.js
@@ -3,7 +3,7 @@
  * 2. Sử dụng method `add` để tính tổng 2 số bất kì
  * 3. Log ra kết quả phép tính tổng ở trên
  */
-var add2 = require("./sum.js").default;
+var math = require("./math.js");
 
-console.log(add2(2, 3));
-console.log(add2(3, 4));
+console.log(math.add(2, 3));
+console.log(math.add(3, 4));
